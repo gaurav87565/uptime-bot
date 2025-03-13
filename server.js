b6ee07bbbce2ea64da0ded91aed8cf1eadfac947
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-13 16:31:07 +0530

    updates

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Bot is online!');
+  res.send('Bot is running!');
 });
 
-app.listen(3000, () => {
-    console.log('✅ Express server running on port 3000');
+app.listen(port, () => {
+  console.log(`✅ Server is running on port ${port}`);
 });
